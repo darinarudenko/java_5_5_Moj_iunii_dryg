0f2fb6284ad89a8e1a8e6337e52c191e31e1bd82
darinarudenko <darynarudenko96@gmail.com>
2021-10-25 21:39:30 +0200

    Task complete

--- src/it/campe/Main.java
+++ src/it/campe/Main.java
@@ -3,6 +3,9 @@ package it.campe;
 public class Main {
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+       int a = 2021;
+       int b = 22;
+       int c = a-b;
+       System.out.println(c);//напишите тут ваш код
     }
 }
